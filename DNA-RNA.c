b7d8315fef14bce88bd3d95681242129b0d0f9f0
Antimatter137 <148151420+antimatter137@users.noreply.github.com>
2026-01-20 00:03:10 -0700

    Add files via upload

--- DNA-RNA.c
+++ DNA-RNA.c
@@ -1,60 +1,72 @@
 #include <stdio.h>
 
+void DNADNA(char *DNASequence) {
+    for(int i = 0; DNASequence[i] != '\0'; i++) {
+        if(DNASequence[i] == 'G'){
+            DNASequence[i] = 'C';
+        }
+        else if(DNASequence[i] == 'C'){
+            DNASequence[i] = 'G';
+        }
+        else if(DNASequence[i] == 'A'){
+            DNASequence[i] = 'T';
+        }
+        else if(DNASequence[i] == 'T'){
+            DNASequence[i] = 'A';
+        }
+    }
+}
+
+void DNARNA(char *DNASequence) {
+    for(int i = 0; DNASequence[i] != '\0'; i++) {
+        if(DNASequence[i] == 'G'){
+            DNASequence[i] = 'C';
+        }
+        else if(DNASequence[i] == 'C'){
+            DNASequence[i] = 'G';
+        }
+        else if(DNASequence[i] == 'A'){
+            DNASequence[i] = 'U';
+        }
+        else if(DNASequence[i] == 'T'){
+            DNASequence[i] = 'A';
+        }
+    }
+}
+
+void RNADNA(char *DNASequence) {
+    for(int i = 0; DNASequence[i] != '\0'; i++) {
+        if(DNASequence[i] == 'G'){
+            DNASequence[i] = 'C';
+        }
+        else if(DNASequence[i] == 'C'){
+            DNASequence[i] = 'G';
+        }
+        else if(DNASequence[i] == 'A'){
+            DNASequence[i] = 'T';
+        }
+        else if(DNASequence[i] == 'U'){
+            DNASequence[i] = 'A';
+        }
+    }
+}
+
 int main() {
     char DNASequence[200];
-    int choise;
+    int choice;
     printf("DNA to DNA: 1\nDNA to RNA: 2\nRNA to DNA: 3\nWhat do you want to do: ");
-    scanf("%d", &choise);
+    scanf("%d", &choice);
     printf("Enter your sequence: ");
     scanf("%s", &DNASequence);
-    switch(choise){
+    switch(choice){
         case 1:
-            for(int i = 0; i < sizeof(DNASequence); i++) {
-                if(DNASequence[i] == 'G'){
-                    DNASequence[i] = 'C';
-                }
-                else if(DNASequence[i] == 'C'){
-                    DNASequence[i] = 'G';
-                }
-                else if(DNASequence[i] == 'A'){
-                    DNASequence[i] = 'T';
-                }
-                else if(DNASequence[i] == 'T'){
-                    DNASequence[i] = 'A';
-                }
-            }
+            DNADNA(DNASequence);
             break;
         case 2:
-            for(int i = 0; i < sizeof(DNASequence); i++) {
-                if(DNASequence[i] == 'G'){
-                    DNASequence[i] = 'C';
-                }
-                else if(DNASequence[i] == 'C'){
-                    DNASequence[i] = 'G';
-                }
-                else if(DNASequence[i] == 'A'){
-                    DNASequence[i] = 'U';
-                }
-                else if(DNASequence[i] == 'T'){
-                    DNASequence[i] = 'A';
-                }
-            }
+            DNARNA(DNASequence);
             break;
         case 3:
-            for(int i = 0; i < sizeof(DNASequence); i++) {
-                if(DNASequence[i] == 'G'){
-                    DNASequence[i] = 'C';
-                }
-                else if(DNASequence[i] == 'C'){
-                    DNASequence[i] = 'G';
-                }
-                else if(DNASequence[i] == 'A'){
-                    DNASequence[i] = 'T';
-                }
-                else if(DNASequence[i] == 'U'){
-                    DNASequence[i] = 'A';
-                }
-            }
+            RNADNA(DNASequence);
             break;
     }
     printf("%s\n", DNASequence);
